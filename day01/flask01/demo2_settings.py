9be9202692262c6fa44f3648881ed7208b8c628a
guoliangmiao <comeonmgl@163.com>
2021-12-28 23:04:19 -0800

    三种配置引入方式

---
+++ day01/flask01/demo2_settings.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+
+# 配置文件加载有三种方式：
+# 1. 从配置对象中加载
+class DefaultConfig(object):
+    MYSQL_URL = "127.0.0.1:6379"
+
+
+#  import_name我们一般会都传入__name__让系统去自动获取入口，如果传入的参数和内件模块名字相同会导致static资源无法访问
+app = Flask(__name__)
+app.config.from_pyfile('setting.ini')  # 从配置文件中读取
+app.config.from_object(DefaultConfig)  # 从配置对象中读取
+# silent表示沉默，是否记载到环境变量的配置信息
+# 如果True表示沉默，加载不到不报错
+# 如果False表示不沉默，加载不到报错
+app.config.from_envvar('SET', silent=True)  # 环境变量set指向的是一个配置文件，获取的方式与其他两种相同
+print(app.config.get("MYSQL_URL"))
+
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+
+if __name__ == "__main__":
+    app.run()
--- day01/flask01/demo_hello.py
+++ day01/flask01/demo_hello.py
@@ -1,8 +1,10 @@
 from flask import Flask
+
 #  import_name我们一般会都传入__name__让系统去自动获取入口，如果传入的参数和内件模块名字相同会导致static资源无法访问
 app = Flask(__name__)
 
 print(__name__)
+
 
 @app.route("/")
 def hello():
